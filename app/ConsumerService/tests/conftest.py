5d3b49715c05f76142fda27c6c3adcb276c1f099
AviMenashe <avi.menashe01@gmail.com>
2021-03-27 14:56:41 +0300

    update test configuration (clean unnecessary code)

--- app/ConsumerService/tests/conftest.py
+++ app/ConsumerService/tests/conftest.py
@@ -5,7 +5,3 @@ def pytest_generate_tests(metafunc):
     os.environ['RABBIT_HOST'] = 'localhost'
     os.environ['RABBIT_USER'] = 'avi'
     os.environ['RABBIT_PASS'] = 'avipass'
-    os.environ['DB_HOST'] = 'localhost'
-    os.environ['DB_NAME'] = 'TEST_clew_medical'
-    os.environ['DB_USER'] = 'avidb'
-    os.environ['DB_PASSWORD'] = 'password'
